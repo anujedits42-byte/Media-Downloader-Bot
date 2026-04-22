1834ab03ae779ea4fcacfc8f8e79bdc0cf129cd0
anujedits42-byte <anujedits42@gmail.com>
2026-04-22 11:20:28 +0530

    Update web.py

--- web.py
+++ web.py
@@ -5,8 +5,3 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Bot is running ✅"
-
-
-# 👇 Ye add karo taaki direct run bhi ho sake
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
